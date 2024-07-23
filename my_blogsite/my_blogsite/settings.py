58437396d2cbee63cbd7491d450c305de17f2f36
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-23 14:13:04 +0300

    forms

--- my_blogsite/my_blogsite/settings.py
+++ my_blogsite/my_blogsite/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ua)ma^&rui_-x_lmyf6l&8g_**xe#m7&1-%$-2r9t#drz5h53f'
+SECRET_KEY = os.getenv("DJANGO_SCRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
